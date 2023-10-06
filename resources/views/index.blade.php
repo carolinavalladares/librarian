2b53a0c0dd6e60575fc124ddbf40a335629af5a0
Carolina Valladares <carolinalimacvalladares@gmail.com>
2023-10-06 20:41:52 -0300

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+
+ @extends('layout')
+
+
+@section('content')
+    <section>
+        <h1 class="text-2xl font-semibold text-center">Bem-vindo ao Librarian</h1>
+
+        <p class="text-center">O seu sistema de gerenciamento de Biblioteca</p>
+    </section>
+@endsection
