54af7a1b26f4764a882a45c690d6bf3366f21926
Carolina Valladares <carolinalimacvalladares@gmail.com>
2023-10-18 22:52:59 -0300

    fix: student filter tabs

--- resources/js/students/students_filter.js
+++ resources/js/students/students_filter.js
@@ -4,14 +4,21 @@ filterLinks.forEach((link) => {
     link.classList.remove("bg-orange-200");
     const query = window.location.search;
     const name = link.getAttribute("name");
+
+    // check what tab should be active
+    if (!query.includes("filter") && name == "all") {
+        link.classList.add("bg-orange-200");
+        return;
+    }
+
     if (!query && name == "all") {
         link.classList.add("bg-orange-200");
         return;
     }
 
-    const filter = window.location.search.split("=")[1];
+    const filter = window.location.search;
 
-    if (filter == name) {
+    if (filter.includes(name)) {
         link.classList.add("bg-orange-200");
     }
 });
